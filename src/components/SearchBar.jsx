fc76ead656f30dfe752c2346f5b78757867a4432
Sbabureddy <sbabureddy01@gmail.com>
2020-03-04 20:03:48 +0530

    fixed filter results

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,48 +1,35 @@
 import React, { Component } from "react";
 
 export class SearchBar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      input: ""
-    };
-  }
+
 
   handleChange = e => {
     this.setState({ input: e.target.value });
     const value = this.state.input;
-    const ulList = document.getElementById("filtered");
 
-    const regex = new RegExp(`^${value}`, "ig");
+
+    const filteredRamen = this.state.restaurants.filter((ramen, index) => {
+
+    })
     const filtered = this.props.restaurants.filter(country =>
       country.Country.match(regex)
     );
-    ulList.innerText = filtered.map(country => `${country.Country}`);
+
   };
 
   render() {
     const { restaurants } = this.props;
+    const { input } = this.state;
+    const filteredRamen = [];
+    if(input && restaurants.length > 0){
+        filteredRamen = restaurants.filter(ramen => ramen.Country.includes(input));
+    }
     return (
       <div>
-        <form>
-          <div className="form-group">
-            <label htmlFor="restaurant">
-              Search for your Favourite Restaurant
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="restaurant"
-              aria-describedby="restaurantHelp"
-              value={this.state.input}
-              onChange={this.handleChange}
-            />
-            <small id="restaurantHelp" className="form-text text-muted">
-              Restaurant list will appear here.
-            </small>
-          </div>
-          <div id="filtered"></div>
-        </form>
+
+        <ul>
+
+        </ul>
       </div>
     );
   }
