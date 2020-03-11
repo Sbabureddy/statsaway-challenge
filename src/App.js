2c819e772ba5be99c6e2fc5ec78929a016d845cd
Sbabureddy <sbabureddy01@gmail.com>
2020-03-11 16:44:25 +0530

    Center Title

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-// import Post from "./components/Post";
 import RestaruntList from "./components/RestaruntList";
 
 function App() {
   return (
     <div className="container">
-      <h2>Ramen List</h2>
+      <h2 className="text-center">Ramen List</h2>
       <RestaruntList />
-      {/* <Post /> */}
     </div>
   );
 }
